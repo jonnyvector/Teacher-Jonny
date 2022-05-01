62092a9b5b78199c597e1c497afccbc7fbcef325
Jonny <jonathan@smartlivingenergy.com>
2022-05-02 01:22:10 +0700

    So many changes

--- script.js
+++ script.js
@@ -82,6 +82,39 @@ document.querySelectorAll(".nav-link").forEach((n) =>
   })
 );
 
+// Get modal element
+var modal = document.getElementById("simpleModal");
+// Get open modal button
+var modalBtn = document.getElementById("modalBtn");
+// Get close button
+var closeBtn = document.getElementById("closeBtn");
+
+// listen for open click
+modalBtn.addEventListener("click", openModal);
+// listen for close click
+closeBtn.addEventListener("click", closeModal);
+
+// listen for outside click
+window.addEventListener("click", outsideClick);
+
+// Function to open modal
+function openModal() {
+  modal.style.display = "block";
+  console.log(123);
+}
+// Function to close modal
+function closeModal() {
+  modal.style.display = "none";
+  console.log(123);
+}
+
+// functin to close modal if outside click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 // payButtons[0].addEventListener("click", function () {
 //   fetch("/create-checkout-session", {
 //     method: "POST",
