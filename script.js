1781d1edfd9d95ad0e1aceb9fb3f1ee365e5d0d0
Jonny <jonathan@smartlivingenergy.com>
2022-05-02 05:17:23 +0700

    Stylings

--- script.js
+++ script.js
@@ -53,14 +53,14 @@ var downArrow = document.getElementById("downArrow");
 
 let x = 0;
 
-upArrow.onclick = function () {
+downArrow.onclick = function () {
   if (x > "-900") {
     x = x - 300;
     slide.style.top = x + "px";
   }
 };
 
-downArrow.onclick = function () {
+upArrow.onclick = function () {
   if (x < 0) {
     x = x + 300;
     slide.style.top = x + "px";
